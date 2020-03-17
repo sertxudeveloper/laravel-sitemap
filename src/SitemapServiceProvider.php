e17d03e9bd30cdfaa8e499c92c827f9a260fdc23
Sertxu Developer <dev.sertxu@gmail.com>
2020-03-17 19:31:09 +0100

    Added Service Provider

---
+++ src/SitemapServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SertxuDeveloper\Sitemap;
+
+use Illuminate\Support\ServiceProvider;
+
+class SitemapServiceProvider extends ServiceProvider {
+
+  public function boot() {
+    $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-sitemap');
+  }
+
+  public function register() {
+    //
+  }
+}
